7a5a62b4a975499f8306eb4c2ae84c244c31d76b
zamir1001 <73525672+zamir1001@users.noreply.github.com>
2020-11-01 03:32:34 +0100

    Update DownloadPNG.cmake

--- cmake/DownloadPNG.cmake
+++ cmake/DownloadPNG.cmake
@@ -6,7 +6,7 @@ INCLUDE(ExternalProject)
 ExternalProject_Add(libpng
 	GIT_REPOSITORY https://github.com/glennrp/libpng.git
 	GIT_TAG master
-	SOURCE_DIR "${DNN_DEPENDENCIES_SOURCE_DIR}/linpng"
+	SOURCE_DIR "${DNN_DEPENDENCIES_SOURCE_DIR}/libpng"
 	BINARY_DIR "${DNN_DEPENDENCIES_BINARY_DIR}/libpng"
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ""
