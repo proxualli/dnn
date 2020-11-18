086a3cb29c268393548a34a7196ae0b15b6786ed
zamir1001 <dhaene.filip@skynet.be>
2020-11-18 01:21:25 +0100

    latest build

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir build
+cd build
+cmake -G Ninja ..
+cp ~/dnn/deps/zlib-1.2.11/zlib.h ~/dnn/build/deps/zlib-1.2.11
+cp ~/dnn/deps/libpng/scripts/pnglibconf.h.prebuilt pnglibconf.h
+ninja zlib
+ninja
+
