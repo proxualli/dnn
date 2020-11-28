53d07bc9b1614fcf55d588f629fc0ebdd829c562
zamir1001 <dhaene.filip@skynet.be>
2020-11-28 11:36:33 +0100

    latest build

--- build.sh
+++ build.sh
@@ -15,6 +15,6 @@ export KMP_SETTINGS=TRUE
 mkdir -p build && cd build && cmake -G Ninja ..
 
 # cp ~/dnn/deps/zlib-1.2.11/zlib.h ~/dnn/build/deps/zlib-1.2.11
-cp ~/dnn/deps/libpng/scripts/pnglibconf.h.prebuilt ~/dnn/build/deps/libpng/pnglibconf.h
+# cp ~/dnn/deps/libpng/scripts/pnglibconf.h.prebuilt ~/dnn/build/deps/libpng/pnglibconf.h
 
 ninja
