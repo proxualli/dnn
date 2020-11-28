dd24b65036e9e618866bd10a64a86a0dcfb37cc1
zamir1001 <dhaene.filip@skynet.be>
2020-11-28 11:44:27 +0100

    latest build

--- build.sh
+++ build.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
 sudo apt-get install nasm
 sudo apt-get install doxygen
 sudo apt-get install cmake
 sudo apt-get install ninja-build
-
-# export CC=clang and export CXX=clang++
+export CC=clang and export CXX=clang++
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export vCPUs=`cat /proc/cpuinfo | grep processor | wc -l`
 export OMP_NUM_THREADS=$((vCPUs / 2))
 export OMP_DISPLAY_ENV=TRUE
 export KMP_SETTINGS=TRUE
-
 mkdir -p build && cd build && cmake -G Ninja ..
-
-# cp ~/dnn/deps/zlib-1.2.11/zlib.h ~/dnn/build/deps/zlib-1.2.11
-# cp ~/dnn/deps/libpng/scripts/pnglibconf.h.prebuilt ~/dnn/build/deps/libpng/pnglibconf.h
-
 ninja
