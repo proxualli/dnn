f3d32a0df1f36ca03d4c749a7e993008f25eb0f2
zamir1001 <dhaene.filip@skynet.be>
2020-11-29 05:45:07 +0100

    latest build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sudo apt-get install nasm
 sudo apt-get install doxygen
 sudo apt-get install cmake
 sudo apt-get install ninja-build
-# export CC=clang and export CXX=clang++
+export CC=clang and export CXX=clang++
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export vCPUs=`cat /proc/cpuinfo | grep processor | wc -l`
 export OMP_NUM_THREADS=$((vCPUs / 2))
