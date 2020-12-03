13dabd7806100a742ac198d372a33b265433dd50
zamir1001 <dhaene.filip@skynet.be>
2020-12-03 15:16:48 +0100

    latest build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sudo apt-get install doxygen
 sudo apt-get install cmake
 sudo apt-get install ninja-build
 
-# export CC=clang and export CXX=clang++
+export CC=clang and export CXX=clang++
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export OMP_DISPLAY_ENV=TRUE
 export KMP_SETTINGS=TRUE
