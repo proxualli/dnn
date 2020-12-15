5c9c7890b5bb2d15fdd9f8a0b5008bccd4ef1fe9
zamir1001 <dhaene.filip@skynet.be>
2020-12-15 19:48:59 +0100

    latest build

--- src/test.cpp
+++ src/test.cpp
@@ -229,7 +229,7 @@ int main()
     auto model = ScriptsCatalog::Generate(p);
 
 
-    const auto optimzer = Optimizers::NAG;
+    const auto optimizer = Optimizers::NAG;
     const auto persistOptimizer = true;
 
     DNNDataprovider(path);
