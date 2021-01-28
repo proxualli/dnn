871677e37836e5f00eb78a59ba2353358e956233
zamir1001 <dhaene.filip@skynet.be>
2021-01-28 07:20:59 +0100

    latest build

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -168,6 +168,8 @@ extern "C" DNN_API bool DNNSetFormat(const bool plain)
 {
 	if (model)
 		return model->SetFormat(plain);
+		
+	return false;
 }
 
 extern "C" DNN_API void DNNGetConfusionMatrix(const size_t costLayerIndex, std::vector<std::vector<size_t>>* confusionMatrix)
