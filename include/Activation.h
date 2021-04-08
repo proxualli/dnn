78c352c3606daf375e0acc3a7998ed91bf3fdf98
zamir1001 <dhaene.filip@skynet.be>
2021-04-08 02:58:35 +0200

    latest build

--- include/Activation.h
+++ include/Activation.h
@@ -1,7 +1,25 @@
 #pragma once
 #include "Layer.h"
 
-#include <immintrin.h>
+#if defined(_MSC_VER)
+/* Microsoft C/C++-compatible compiler */
+#include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+/* GCC-compatible compiler, targeting x86/x86-64 */
+#include <x86intrin.h>
+#elif defined(__GNUC__) && defined(__ARM_NEON__)
+/* GCC-compatible compiler, targeting ARM with NEON */
+#include <arm_neon.h>
+#elif defined(__GNUC__) && defined(__IWMMXT__)
+/* GCC-compatible compiler, targeting ARM with WMMX */
+#include <mmintrin.h>
+#elif (defined(__GNUC__) || defined(__xlC__)) && (defined(__VEC__) || defined(__ALTIVEC__))
+/* XLC or GCC-compatible compiler, targeting PowerPC with VMX/VSX */
+#include <altivec.h>
+#elif defined(__GNUC__) && defined(__SPE__)
+/* GCC-compatible compiler, targeting PowerPC with SPE */
+#include <spe.h>
+#endif
 
 namespace dnn
 {
@@ -143,7 +161,6 @@ namespace dnn
 	 
 	struct TanhExp
 	{
-		
 		inline static Float f(const Float& x) noexcept { return x * std::tanh(std::exp(x)); }
 		inline static Float df(const Float& x) noexcept { const auto y = std::exp(x);  const auto z = std::tanh(y); return z - (x * y * (FloatSquare(z) - Float(1))); }
 		inline static VecFloat fVec(const VecFloat& x) noexcept { return x * VecFloat(_mm256_tanh_ps(exp(x).operator __m256())); }
