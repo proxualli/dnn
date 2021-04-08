60088b76d347b9c5eeac387eb5b36be7701953c8
zamir1001 <dhaene.filip@skynet.be>
2021-04-08 22:36:59 +0200

    latest build

--- src/test.cpp
+++ src/test.cpp
@@ -210,10 +210,6 @@ int __cdecl wmain(int argc, wchar_t* argv[])
 int main(int argc, char* argv[])
 #endif
 {
-    auto a = dnn::TanhExp::fVec(0.52f);
-    auto b = dnn::TanhExp::dfVec(0.52f);
-    std::cout << std::to_string(a[0]) << std::endl << std::to_string(b[0]) << std::endl;
-
     CheckMsg msg;
 
     ScriptParameters p;
@@ -239,7 +235,7 @@ int main(int argc, char* argv[])
 
     const auto optimizer = Optimizers::NAG;
     const auto persistOptimizer = true;
-    /*
+   
     DNNDataprovider(path);
     
     if (DNNReadDefinition(model, optimizer, msg) == 1)
@@ -294,6 +290,4 @@ int main(int argc, char* argv[])
     }
     else
         std::cout << std::endl << "Could not load model" << std::endl << msg.Message << std::endl << model << std::endl;
-
-        */
 }
