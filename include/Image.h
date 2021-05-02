8aac9a4412e7372db86a5a4a2756ff375fc6185d
zamir1001 <dhaene.filip@skynet.be>
2021-05-02 03:03:44 +0200

    latest build

--- include/Image.h
+++ include/Image.h
@@ -776,9 +776,13 @@ namespace dnn
 						if (Bernoulli<bool>(dropout))
 							for (auto c = 0ull; c < dstImage.Channels; c++)
 								if constexpr (std::is_floating_point_v<T>)
+								{
 									dstImage(c, d, h, w) = T(0);
+								}
 								else
+								{
 									dstImage(c, d, h, w) = T(mean[c]);
+								}
 
 			return dstImage;
 		}
