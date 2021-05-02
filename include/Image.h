85ace834323e2a8c1b3f83f43d7250fab2abbdb8
zamir1001 <dhaene.filip@skynet.be>
2021-05-02 03:32:19 +0200

    latest build

--- include/Image.h
+++ include/Image.h
@@ -33,10 +33,10 @@ namespace dnn
 	template<typename T>
 	struct Image
 	{
-		typedef AlignedVector<T, 64ull> ArrayT;
+		typedef AlignedVector<T, 64ull> VectorT;
 
 	private:
-		ArrayT Data;
+		VectorT Data;
 
 	public:
 		UInt Channels;
@@ -49,11 +49,11 @@ namespace dnn
 			Depth(0),
 			Height(0),
 			Width(0),
-			Data(ArrayT())
+			Data(VectorT())
 		{
 		}
 
-		Image(const UInt c, const UInt d, const UInt h, const UInt w, const ArrayT& image) :
+		Image(const UInt c, const UInt d, const UInt h, const UInt w, const VectorT& image) :
 			Channels(c),
 			Depth(d),
 			Height(h),
@@ -67,7 +67,7 @@ namespace dnn
 			Depth(d),
 			Height(h),
 			Width(w),
-			Data(ArrayT(c * d * h * w))
+			Data(VectorT(c * d * h * w))
 		{
 			std::memcpy(Data.data(), image, c * d * h * w * sizeof(T));
 		}
@@ -77,7 +77,7 @@ namespace dnn
 			Depth(d),
 			Height(h),
 			Width(w),
-			Data(ArrayT(c * d * h * w))
+			Data(VectorT(c * d * h * w))
 		{
 		}
 
