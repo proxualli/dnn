68ffd4a80c5c968ec5dc40e3085b3e77ab8ad71f
zamir1001 <dhaene.filip@skynet.be>
2021-06-21 23:44:03 +0200

    latest build

--- test/testers/batchnormactivation.h
+++ test/testers/batchnormactivation.h
@@ -11,20 +11,22 @@
 #include <functional>
 #include <algorithm>
 
-#include <nnpack.h>
-#include <nnpack/reference.h>
-#include <nnpack/AlignedAllocator.h>
+#include <Model.h>
 
 
-class BatchNormActivationTester {
+class BatchNormActivationTester
+{
 public:
 	BatchNormActivationTester() :
 		iterations_(1),
 		errorLimit_(1.0e-5),
 		batchSize_(1),
 		channels_(1),
-        height_(1),
-        width_(1)
+		height_(1),
+		width_(1),
+		activation_(dnn::Activations::HardSwish),
+		layerType_(dnn::LayerTypes::BatchNormHardSwish),
+		device_(dnn::Device(dnnl::engine(dnnl::engine::kind::cpu, 0), dnnl::stream(dnnl::engine(dnnl::engine::kind::cpu, 0), dnnl::stream::flags::default_flags)))
 	{
 	}
 
@@ -36,77 +38,121 @@ public:
 		batchSize_(tester.batchSize_),
 		channels_(tester.channels_),
 		width_(tester.width_),
-		height_(tester.height_)
+		height_(tester.height_),
+		activation_(tester.activation_),
+		layerType_(tester.layerType_),
+		device_(tester.device_)
 	{
 	}
 
 	BatchNormActivationTester& operator=(const BatchNormActivationTester&) = delete;
 
-	~BatchNormActivationTester() {
+	~BatchNormActivationTester()
+	{
 	}
 
-	inline BatchNormActivationTester& iterations(size_t iterations) {
+	inline BatchNormActivationTester& iterations(size_t iterations)
+	{
 		this->iterations_ = iterations;
 		return *this;
 	}
 
-	inline size_t iterations() const {
+	inline size_t iterations() const
+	{
 		return this->iterations_;
 	}
 
-	inline BatchNormActivationTester& errorLimit(float errorLimit) {
+	inline BatchNormActivationTester& errorLimit(float errorLimit)
+	{
 		this->errorLimit_ = errorLimit;
 		return *this;
 	}
 
-	inline float errorLimit() const {
+	inline float errorLimit() const
+	{
 		return this->errorLimit_;
 	}
 
-	inline BatchNormActivationTester& batchSize(size_t batchSize) {
+	inline BatchNormActivationTester& batchSize(size_t batchSize)
+	{
 		this->batchSize_ = batchSize;
 		return *this;
 	}
 
-	inline size_t batchSize() const {
+	inline size_t batchSize() const
+	{
 		return this->batchSize_;
 	}
 
-	inline BatchNormActivationTester& channels(size_t channels) {
+	inline BatchNormActivationTester& channels(size_t channels)
+	{
 		this->channels_ = channels;
 		return *this;
 	}
 
-	inline size_t channels() const {
+	inline size_t channels() const
+	{
 		return this->channels_;
 	}
 
-    inline BatchNormActivationTester& height(size_t height) {
+	inline BatchNormActivationTester& height(size_t height)
+	{
 		this->height_ = height;
 		return *this;
 	}
 
-	inline size_t height() const {
-		return this->_height;
+	inline size_t height() const
+	{
+		return this->height_;
 	}
 
-    inline BatchNormActivationTester& width(size_t width) {
+	inline BatchNormActivationTester& width(size_t width)
+	{
 		this->width_ = width;
 		return *this;
 	}
 
-	inline size_t width() const {
-		return this->_width;
+	inline size_t width() const
+	{
+		return this->width_;
 	}
 
-	void testForward(bool inference, enum nnp_activation activation = nnp_activation_identity) const {
+	inline BatchNormActivationTester& layerType(dnn::LayerTypes layerType)
+	{
+		this->layerType_ = layerType;
+		return *this;
+	}
+
+	inline dnn::LayerTypes layerType() const
+	{
+		return this->layerType_;
+	}
+
+	inline BatchNormActivationTester& activation(dnn::Activations activation)
+	{
+		this->activation_ = activation;
+		return *this;
+	}
+
+	inline dnn::Activations activation() const
+	{
+		return this->activation_;
+	}
+
+	inline dnn::Device device() const
+	{
+		return this->device_;
+	}
+		
+	void testForward(bool inference) const
+	{
 		const uint_fast32_t seed = std::chrono::system_clock::now().time_since_epoch().count();
 		auto rng = std::bind(std::uniform_real_distribution<float>(-0.1, 1.0), std::mt19937(seed));
 
 		std::vector<float> input(batchSize() * channels() * height() * width());
 		std::vector<float> output(batchSize() * channels() * height() * width());
 		std::vector<float> referenceOutput(batchSize() * channels() * height() * width());
-
+				
 		size_t scratchSize = 0;
 		enum nnp_status status = nnp_convolution_output(
 			algorithm,
@@ -163,113 +209,7 @@ public:
 		EXPECT_LT(median(maxErrors), errorLimit());
 	}
 
-	void testInputGradient(enum nnp_convolution_algorithm algorithm, enum nnp_activation activation = nnp_activation_identity) const {
-		const uint_fast32_t seed = std::chrono::system_clock::now().time_since_epoch().count();
-		auto rng = std::bind(std::uniform_real_distribution<float>(), std::mt19937(seed));
-
-		std::vector<float> outputGradient(batchSize() * outputChannels() * outputHeight() * outputWidth());
-		std::vector<float> kernel(outputChannels() * inputChannels() * kernelHeight() * kernelWidth());
-
-		std::vector<float> inputGradient(batchSize() * inputChannels() * inputHeight() * inputWidth());
-
-		std::vector<float> referenceInputGradient(batchSize() * inputChannels() * inputHeight() * inputWidth());
-
-		size_t scratchSize = 0;
-		enum nnp_status status = nnp_convolution_input_gradient(
-			algorithm,
-			batchSize(), inputChannels(), outputChannels(),
-			inputSize(), inputPadding(), kernelSize(),
-			nullptr, nullptr, nullptr, nullptr, &scratchSize,
-			nnp_activation_identity, nullptr,
-			this->threadpool, nullptr);
-		ASSERT_EQ(nnp_status_success, status);
-
-		std::vector<uint8_t, AlignedAllocator<uint8_t, 64>> scratchBuffer(scratchSize);
-		std::vector<float> maxErrors;
-		for (size_t iteration = 0; iteration < iterations(); iteration++) {
-			std::generate(outputGradient.begin(), outputGradient.end(), std::ref(rng));
-			std::generate(kernel.begin(), kernel.end(), std::ref(rng));
-			std::fill(inputGradient.begin(), inputGradient.end(), nanf(""));
-			std::fill(scratchBuffer.begin(), scratchBuffer.end(), 0xA5);
-
-			nnp_convolution_input_gradient__reference(
-				batchSize(), inputChannels(), outputChannels(),
-				inputSize(), inputPadding(), kernelSize(),
-				outputGradient.data(), kernel.data(), referenceInputGradient.data(),
-				this->threadpool);
-
-			enum nnp_status status = nnp_convolution_input_gradient(
-				algorithm,
-				batchSize(), inputChannels(), outputChannels(),
-				inputSize(), inputPadding(), kernelSize(),
-				outputGradient.data(), kernel.data(), inputGradient.data(),
-				scratchSize == 0 ? nullptr : scratchBuffer.data(),
-				scratchSize == 0 ? nullptr : &scratchSize,
-				nnp_activation_identity, NULL,
-				this->threadpool, nullptr);
-			ASSERT_EQ(nnp_status_success, status);
-
-			const float maxError = std::inner_product(referenceInputGradient.cbegin(), referenceInputGradient.cend(), inputGradient.cbegin(), 0.0f,
-				[](float x, float y)->float { return std::max<float>(y, x); }, relativeError);
-			maxErrors.push_back(maxError);
-		}
-		EXPECT_LT(median(maxErrors), errorLimit());
-	}
-
-	void testKernelGradient(enum nnp_convolution_algorithm algorithm, enum nnp_activation activation = nnp_activation_identity) const {
-		const uint_fast32_t seed = std::chrono::system_clock::now().time_since_epoch().count();
-		auto rng = std::bind(std::uniform_real_distribution<float>(), std::mt19937(seed));
-
-		std::vector<float> input(batchSize() * inputChannels() * inputHeight() * inputWidth());
-		std::vector<float> outputGradient(batchSize() * outputChannels() * outputHeight() * outputWidth());
-		std::vector<float> kernelGradient(outputChannels() * inputChannels() * kernelHeight() * kernelWidth());
-
-		std::vector<float> referenceKernelGradient(outputChannels() * inputChannels() * kernelHeight() * kernelWidth());
-
-		size_t scratchSize = 0;
-		enum nnp_status status = nnp_convolution_kernel_gradient(
-			algorithm,
-			batchSize(), inputChannels(), outputChannels(),
-			inputSize(), inputPadding(), kernelSize(),
-			nullptr, nullptr, nullptr, nullptr, &scratchSize,
-			nnp_activation_identity, nullptr,
-			this->threadpool, nullptr);
-		ASSERT_EQ(nnp_status_success, status);
-
-		std::vector<uint8_t, AlignedAllocator<uint8_t, 64>> scratchBuffer(scratchSize);
-		std::vector<float> maxErrors;
-		for (size_t iteration = 0; iteration < iterations(); iteration++) {
-			std::generate(input.begin(), input.end(), std::ref(rng));
-			std::generate(outputGradient.begin(), outputGradient.end(), std::ref(rng));
-			std::fill(kernelGradient.begin(), kernelGradient.end(), nanf(""));
-			std::fill(scratchBuffer.begin(), scratchBuffer.end(), 0xA5);
-
-			nnp_convolution_kernel_gradient__reference(
-				batchSize(), inputChannels(), outputChannels(),
-				inputSize(), inputPadding(), kernelSize(),
-				input.data(), outputGradient.data(), referenceKernelGradient.data(),
-				this->threadpool);
-
-			enum nnp_status status = nnp_convolution_kernel_gradient(
-				algorithm,
-				batchSize(), inputChannels(), outputChannels(),
-				inputSize(), inputPadding(), kernelSize(),
-				input.data(), outputGradient.data(), kernelGradient.data(),
-				scratchSize == 0 ? nullptr : scratchBuffer.data(),
-				scratchSize == 0 ? nullptr : &scratchSize,
-				nnp_activation_identity, NULL,
-				this->threadpool,
-				NULL);
-			ASSERT_EQ(nnp_status_success, status);
-
-			const float maxError = std::inner_product(referenceKernelGradient.cbegin(), referenceKernelGradient.cend(), kernelGradient.cbegin(), 0.0f,
-				[](float x, float y)->float { return std::max<float>(y, x); }, relativeError);
-			maxErrors.push_back(maxError);
-		}
-		EXPECT_LT(median(maxErrors), errorLimit());
-	}
-
-	void testInference(enum nnp_convolution_algorithm algorithm, enum nnp_activation activation = nnp_activation_identity, bool precompute = false) const {
+	void testBackward() const {
 		ASSERT_EQ(1, batchSize());
 
 		const uint_fast32_t seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -372,11 +312,13 @@ public:
 	}
 
 private:
-	inline static float relativeError(float reference, float actual) {
+	inline static float relativeError(float reference, float actual)
+	{
 		return std::abs(reference - actual) / std::max(FLT_MIN, std::abs(reference));
 	}
 
-	inline static float median(std::vector<float>& array) {
+	inline static float median(std::vector<float>& array)
+	{
 		std::nth_element(array.begin(), array.begin() + array.size() / 2, array.end());
 		return array[array.size() / 2];
 	}
@@ -387,4 +329,7 @@ private:
 	size_t channels_;
 	size_t width_;
     size_t height_;
+	dnn::Activations activation_;
+	dnn::LayerTypes layerType_;
+	dnn::Device device_;
 };
