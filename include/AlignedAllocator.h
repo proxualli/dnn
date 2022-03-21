5772c1ba26a4a909118b0349bca31065c9d3f3a6
zamir1001 <dhaene.filip@skynet.be>
2022-03-21 23:55:24 +0100

    latest build

--- include/AlignedAllocator.h
+++ include/AlignedAllocator.h
@@ -11,6 +11,14 @@
 
 namespace dnn
 {
+	std::size_t DIV(std::size_t align, std::size_t size)
+	{
+		if (size % align == 0ull)
+			return size;
+
+		return ((size / align) + 1ull) * align;
+	}
+
 	template <typename T, std::size_t alignment>
 	class AlignedAllocator
 	{
@@ -83,13 +91,13 @@ namespace dnn
 		inline void* AlignedAlloc(const size_type align, const size_type size) const
 		{
 #if defined(_WIN32) || defined(__CYGWIN__)
-			return ::_aligned_malloc(size, align);
+			return ::_aligned_malloc(DIV(align, size), align);
 #elif defined(__ANDROID__)
-			return ::memalign(align, size);
+			return ::memalign(align, DIV(align, size));
 #elif defined(__MINGW32__)
-			return ::_mm_malloc(size, align);
+			return ::_mm_malloc(DIV(align, size), align);
 #else  // posix assumed
-			return ::aligned_alloc(align, size);
+			return ::aligned_alloc(align, DIV(align, size));
 #endif
 		}
 
