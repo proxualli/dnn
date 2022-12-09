c531e20ba73996cf41acb15afb4d75d6196bff7e
zamir1001 <dhaene.filip@skynet.be>
2022-12-09 19:01:53 +0100

    latest build

--- include/Utils.h
+++ include/Utils.h
@@ -498,6 +498,8 @@ namespace
 	constexpr auto inline GetColorFromRange(const T& range, const T& minimum, const T& value) NOEXCEPT { return Saturate<T>(T(255) - ((value - minimum) * range)); }
 	template<typename T>
 	constexpr auto inline GetColorRange(const T& min, const T& max) NOEXCEPT { return (min == max) ? T(0) : T(255) / ((std::signbit(min) && std::signbit(max)) ? -(min + max) : (max - min)); }
+	
+	/* https://en.wikipedia.org/wiki/Kahan_summation_algorithm */
 	template<typename T>
 	inline void KahanSum(const T& value, T& sum, T& correction) NOEXCEPT
 	{
