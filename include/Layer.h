4f33a8093eafd1d27fd11a9581ee6b5e948b18b3
zamir1002 <dhaene.filip@skynet.be>
2023-02-22 20:00:30 +0100

    latest build

--- include/Layer.h
+++ include/Layer.h
@@ -235,7 +235,7 @@ namespace dnn
 		{
 			if constexpr (Inplace)
 			{
-				if ((layerType == LayerTypes::Activation || layerType == LayerTypes::LayerNorm || layerType == LayerTypes::BatchNormActivation || layerType == LayerTypes::BatchNormActivationDropout || layerType == LayerTypes::BatchNormRelu) &&
+				if ((layerType == LayerTypes::Activation || layerType == LayerTypes::LayerNorm || layerType == LayerTypes::BatchNorm || layerType == LayerTypes::BatchNormActivation || layerType == LayerTypes::BatchNormActivationDropout || layerType == LayerTypes::BatchNormRelu) &&
 					(inputs.size() == 1) &&
 					(inputs[0]->LayerType == LayerTypes::Convolution || inputs[0]->LayerType == LayerTypes::DepthwiseConvolution || inputs[0]->LayerType == LayerTypes::ConvolutionTranspose))
 					return true;
