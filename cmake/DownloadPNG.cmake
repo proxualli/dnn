2d43366ab23462625d6c9d26f381e0632146f7f5
proxualli <139600525+proxualli@users.noreply.github.com>
2023-12-09 09:53:00 +0100

    Update DownloadPNG.cmake

--- cmake/DownloadPNG.cmake
+++ cmake/DownloadPNG.cmake
@@ -5,7 +5,7 @@ PROJECT(png-download NONE)
 INCLUDE(ExternalProject)
 ExternalProject_Add(png
 	GIT_REPOSITORY https://github.com/glennrp/libpng.git
-	GIT_TAG master
+	GIT_TAG 1.6.40
 	SOURCE_DIR "${DNN_DEPENDENCIES_SOURCE_DIR}/libpng"
 	BINARY_DIR "${DNN_DEPENDENCIES_BINARY_DIR}/libpng"
 	CONFIGURE_COMMAND ""
