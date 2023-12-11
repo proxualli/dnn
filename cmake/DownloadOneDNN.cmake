5becf7b4f9b8ce7ced8474bd1245fa1e561f91ff
proxualli <139600525+proxualli@users.noreply.github.com>
2023-12-11 22:35:53 +0100

    Update DownloadOneDNN.cmake

--- cmake/DownloadOneDNN.cmake
+++ cmake/DownloadOneDNN.cmake
@@ -5,7 +5,7 @@ PROJECT(dnnl-download NONE)
 INCLUDE(ExternalProject)
 ExternalProject_Add(dnnl
 	GIT_REPOSITORY https://github.com/oneapi-src/oneDNN.git
-	GIT_TAG master
+	GIT_TAG main
 	SOURCE_DIR "${DNN_DEPENDENCIES_SOURCE_DIR}/oneDNN"
 	BINARY_DIR "${DNN_DEPENDENCIES_BINARY_DIR}/oneDNN"
 	CONFIGURE_COMMAND ""
