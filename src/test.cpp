a86594fe6ba76a02e9d679d2059190740ffe82ef
proxualli <dhaene.filip@skynet.be>
2023-12-26 22:39:48 +0100

    latest build

--- src/test.cpp
+++ src/test.cpp
@@ -234,7 +234,7 @@ int main(int argc, char* argv[])
     p.SqueezeExcitation = true;
     p.ChannelZeroPad = false;
     p.EfficientNet = { { 1, 24, 2, 1, false }, { 4, 48, 4, 2, false }, { 4, 64, 4, 2, false }, { 4, 128, 6, 2, true }, { 6, 160, 9, 1, true }, { 6, 256, 15, 2, true } };
-    p.ShuffleNet = { { 3, 3, 1, 2, false }, { 3, 3, 1, 2, false }, { 7, 3, 1, 2, false } };
+    p.ShuffleNet = { { 3, 3, 1, 2, false }, { 3, 3, 1, 2, false }, { 3, 3, 1, 2, false } };
     p.WeightsFiller = scripts::Fillers::HeNormal;
     p.WeightsFillerMode = scripts::FillerModes::In;
     p.StrideHFirstConv = 1;
