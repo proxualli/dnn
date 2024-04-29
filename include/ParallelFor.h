f7004a5878321155da091a1dc83d15807dce8c1b
proxualli <dhaene.filip@skynet.be>
2024-04-29 02:24:17 +0200

    latest build

--- include/ParallelFor.h
+++ include/ParallelFor.h
@@ -179,7 +179,7 @@ namespace dnn
 					f(i);
 			}
 	#else
-            #pragma omp parallel for schedule(static,1) num_threads(static_cast<int>(threads))
+            #pragma omp parallel for num_threads(static_cast<int>(threads))
 			for (auto i = 0ull; i < range; i++)
 				f(i);
 	#endif
