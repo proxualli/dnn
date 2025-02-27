623641ebfc5c4f7ee0153c1b9f4926c32f3aac8f
uapconvnet <dhaenefilip99@gmail.com>
2025-02-27 03:30:16 +0100

    latest build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade
-sudo apt install build-essential curl gzip unzip libomp-dev clang nasm graphviz doxygen python3-sphinx cmake ninja-build
+sudo apt install build-essential curl unzip gzip libomp-dev clang nasm graphviz doxygen python3-sphinx cmake ninja-build
 
 export CC=clang and export CXX=clang++
 export KMP_AFFINITY=granularity=fine,compact,1,0
@@ -8,6 +8,7 @@ export OMP_DISPLAY_ENV=TRUE
 export KMP_SETTINGS=TRUE
 export KMP_BLOCKTIME=0
 export vCPUs=`cat /proc/cpuinfo | grep processor | wc -l`
-export OMP_NUM_THREADS=$((vCPUs / 2))
+export OMP_NUM_THREADS=$(vCPUs)
+export ONEDNN_EXPERIMENTAL_BNORM_STATS_ONE_PASS=1
 
 rm -f -r ./build && mkdir -p build && cd build && cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON && ninja
